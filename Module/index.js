5164b7bde7f66739eb1f93d067ee9d2b8e374ee2
Mohammad Massoud <36207910+massoudsalem@users.noreply.github.com>
2023-05-22 17:40:48 +0300

    Module Pattern

---
+++ Module/index.js
@@ -0,0 +1,7 @@
+//this pattern splits up your code into smaller, reusable pieces
+import { sum, subtract, divide, multiply } from './math.js';
+
+console.log('Sum', sum(1, 2));
+console.log('Subtract', subtract(1, 2));
+console.log('Divide', divide(1, 2));
+console.log('Multiply', multiply(1, 2));
